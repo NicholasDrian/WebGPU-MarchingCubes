eccefbf2b0cb074c6c0e4ef13238b2686e263813
Nick Drian <nickdrian@Nicks-MacBook-Air.local>
2023-09-17 15:49:30 -0700

    added marching cubes pipeline

--- src/compute.ts
+++ src/compute.ts
@@ -11,13 +11,17 @@ export class MeshGenerator {
 
 	
 	private device!: GPUDevice;
-	private shader!: GPUShaderModule;
-	private bindGroupLayout!: GPUBindGroupLayout;
-	private bindGroup!: GPUBindGroup;
+	private perlinNoiseShader!: GPUShaderModule;
+	private marchingCubesShader!: GPUShaderModule;
+	private perlinNoiseBindGroupLayout!: GPUBindGroupLayout;
+	private perlinNoiseBindGroup!: GPUBindGroup;
+	private marchingCubesBindGroupLayout!: GPUBindGroupLayout;
+	private marchingCubesBindGroup!: GPUBindGroup;
 	private sampleBuffer!: GPUBuffer;
 	private dimensionsBuffer!: GPUBuffer;
 	private sparseMeshBuffer!: GPUBuffer;
-	private computePipeline!: GPUComputePipeline;
+	private perlinNoisePipeline!: GPUComputePipeline;
+	private marchingCubesPipeline!: GPUComputePipeline;
 
 	constructor() {
 
@@ -36,28 +40,55 @@ export class MeshGenerator {
 	}
 
 	private createResources() {
-		this.shader = this.device.createShaderModule({
-			label: "compute shader module",
+		this.perlinNoiseShader = this.device.createShaderModule({
+			label: "perlin noise shader",
 			code: perlinNoiseShader, 
 		});
-		this.bindGroupLayout = this.device.createBindGroupLayout({
+		this.marchingCubesShader = this.device.createShaderModule({
+			label: "marching cubes shader",
+			code: marchingCubesShader,
+		});
+		this.perlinNoiseBindGroupLayout = this.device.createBindGroupLayout({
+			label: "perlin noise bind group layout",
 			entries: [
 				{
-					binding: 0,
+					binding: 0, // perlin noise sample buffer
 					visibility: GPUShaderStage.COMPUTE,
 					buffer: { type: "storage" }
 				},
 				{
-					binding: 1,
+					binding: 1, // uniform parameters
 					visibility: GPUShaderStage.COMPUTE,
-					buffer: { type: "read-only-storage" }
+					buffer: { type: "read-only-storage" } // TODO: change to uniform
 				}
 			]
 		});
+
+		this.marchingCubesBindGroupLayout = this.device.createBindGroupLayout({
+			label: "marching cubes bind group layout",
+			entries: [
+				{
+					binding: 0, // perlin noise sample buffer
+					visibility: GPUShaderStage.COMPUTE,
+					buffer: { type: "read-only-storage" }
+				},
+				{
+					binding: 1, // sparse mesh buffer
+					visibility: GPUShaderStage.COMPUTE,
+					buffer: { type: "storage" }
+				}
+			]
+		});
+		
 		this.sampleBuffer = this.device.createBuffer({
 			label: "sample buffer",
 			size: xSamples * ySamples * zSamples * 4,
-			usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
+			usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC, // COPY_SRC is just for debug
+		});
+		this.sparseMeshBuffer = this.device.createBuffer({
+			label: "sparse mesh buffer",
+			size: (xSamples - 1) * (ySamples - 1) * (zSamples - 1) * 6 * 3 * 4,
+			usage: GPUBufferUsage.STORAGE
 		});
 		this.dimensionsBuffer = this.device.createBuffer({
 			label: "sample dimensions buffer",
@@ -68,9 +99,9 @@ export class MeshGenerator {
 		new Uint32Array(this.dimensionsBuffer.getMappedRange()).set([xSamples, ySamples, zSamples]);
 		this.dimensionsBuffer.unmap();
 
-		this.bindGroup = this.device.createBindGroup({
+		this.perlinNoiseBindGroup = this.device.createBindGroup({
 			label: "compute bind group",
-			layout: this.bindGroupLayout,
+			layout: this.perlinNoiseBindGroupLayout,
 			entries: [
 				{ 
 					binding: 0,
@@ -83,22 +114,50 @@ export class MeshGenerator {
 			]
 		});
 
+		this.marchingCubesBindGroup = this.device.createBindGroup({
+			label: "marching cubes bind group",
+			layout: this.marchingCubesBindGroupLayout,
+			entries: [
+				{
+					binding: 0,
+					resource: { buffer: this.sampleBuffer }
+				}, {
+					binding: 1,
+					resource: { buffer: this.sparseMeshBuffer }
+				}
+			]
+		});
+
 
 	}
 
 	private createPipeline() {
 
-		const computePipelineLayout = this.device.createPipelineLayout({
-			bindGroupLayouts: [this.bindGroupLayout],
+		const perlinNoisePipelineLayout = this.device.createPipelineLayout({
+			bindGroupLayouts: [this.perlinNoiseBindGroupLayout],
 		});
-		this.computePipeline = this.device.createComputePipeline({ // probably want to change this to async...
-			label: "compute pipeline",
-			layout: computePipelineLayout,
+		const marchingCubesPipelineLayout = this.device.createPipelineLayout({
+			bindGroupLayouts: [this.marchingCubesBindGroupLayout],
+		});
+		this.perlinNoisePipeline = this.device.createComputePipeline({ // probably want to change this to async...
+			label: "perlin noise pipeline",
+			layout: perlinNoisePipelineLayout,
 			compute: {
-				module: this.shader,
+				module: this.perlinNoiseShader,
 				entryPoint: "main",
 				constants: {
 				//	blockSize: 1,
+				}
+			}
+		});
+		this.marchingCubesPipeline = this.device.createComputePipeline({
+			label: "marching cubes pipeline",
+			layout: marchingCubesPipelineLayout,
+			compute: {
+				module: this.marchingCubesShader,
+				entryPoint: "main",
+				constants: {
+					// TODO
 				}
 			}
 		});
@@ -118,10 +177,11 @@ export class MeshGenerator {
 		const encoder = this.device.createCommandEncoder();
 
 		const computePass = encoder.beginComputePass();
-		computePass.setPipeline(this.computePipeline);
-		computePass.setBindGroup(0, this.bindGroup);
+		computePass.setPipeline(this.perlinNoisePipeline);
+		computePass.setBindGroup(0, this.perlinNoiseBindGroup);
 		computePass.dispatchWorkgroups(xSamples, ySamples, zSamples);
 		computePass.end();
+
 
 		encoder.copyBufferToBuffer(this.sampleBuffer, 0, debugBuffer, 0, xSamples * ySamples * zSamples * 4);
 
